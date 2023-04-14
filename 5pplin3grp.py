26d6720bfcd0eadc7675e3612b19b0e05a551534
2deadmen <kartikhegde.2002@gmail.com>
2023-04-14 21:23:19 +0530

    binary treeee

--- 5pplin3grp.py
+++ 5pplin3grp.py
@@ -1,22 +1,23 @@
 import random
 
 list=[1,2,3,4,5]
-list1=[]
-list2=[]
-list3=[]
-lists=[list1,list2,list3]
-prev=[1] 
-for i in list:
-    l=random.choice(lists)
-    prev.append(l)
-    while True:
-         if prev[i-1]==l or len(l)>=2:
-            l=random.choice(lists)
-         else:
-            break
-    l.append(i)
+print(list[1:])
+# list1=[]
+# list2=[]
+# list3=[]
+# lists=[list1,list2,list3]
+# prev=[1] 
+# for i in list:
+#     l=random.choice(lists)
+#     prev.append(l)
+#     while True:
+#          if prev[i-1]==l or len(l)>=2:
+#             l=random.choice(lists)
+#          else:
+#             break
+#     l.append(i)
 
-print(list1)
-print(list2)
-print(list3)
+# print(list1)
+# print(list2)
+# print(list3)
     
--- tree/binarytree.py
+++ tree/binarytree.py
@@ -3,6 +3,41 @@ class Tree:
         self.value=value
         self.left=None
         self.right=None
+
+    
+def insert(root,value):
+        list=[]
+        list.append(root)
+        while list!=[]:
+            root=list[0]
+            list=list[1:]
+            if root.left==None:
+                root.left=Tree(value)
+                break
+            if root.right==None:
+                root.right=Tree(value)
+                break
+            list.append(root.left)
+            list.append(root.right)
+
+def delete(root,value):
+        list=[]
+        list.append(root)
+        while list!=[]:
+            root=list[0]
+            list=list[1:]
+            if root.left.value==value:
+                root.left=None
+                break
+            if root.right.value==value:
+                root.right=None
+                break
+            if root.left != None:
+                list.append(root.left)
+            if root.right != None:
+                list.append(root.right) 
+
+
 tree=Tree(1)
 left=Tree(2)
 right=Tree(3)
@@ -12,6 +47,8 @@ subleft=Tree(4)
 left.left=subleft
 subright=Tree(5)
 left.right=subright
+
+
 
 
 def preorderTraverse(root):
@@ -38,9 +75,33 @@ def postorderTraverse(root):
     postorderTraverse(root.right)
     print(root.value)
 
+def lvlorder(root):
+    if not root:
+        return
+    list=[]
+    list.append(root)
+    while list!=[]:
+        root=list[0]
+        print(root.value)
+        list=list[1:]
+        if root.left != None:
+            list.append(root.left)
+        if root.right != None:
+            list.append(root.right)
+       
 
-preorderTraverse(tree)
+
+
+lvlorder(tree)
+insert(tree,6)
 print("\n")
-postorderTraverse(tree)
+lvlorder(tree)
+delete(tree,6)
 print("\n")
-inorderTraverse(tree)
+
+lvlorder(tree)
+# preorderTraverse(tree)
+# print("\n")
+# postorderTraverse(tree)
+# print("\n")
+# inorderTraverse(tree)
